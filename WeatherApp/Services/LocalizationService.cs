570884cc347d01b0af9d3d0a87c6c4627fe7e1eb
Pruthvi <pruthvisoni0505@gmail.com>
2023-10-22 18:05:12 -0400

    Add project files.

---
+++ WeatherApp/Services/LocalizationService.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace WeatherApp.NETMAUI.Services
+{
+    public class LocalizationService
+    {
+        public LocalizationService()
+        {
+
+        }
+        public async Task<Models.Coordinates> GetLocation()
+        {
+            Models.Coordinates coordinates = new Models.Coordinates();
+            try
+            {
+                var request = new Xamarin.Essentials.GeolocationRequest(Xamarin.Essentials.GeolocationAccuracy.Medium);
+                var location = await Xamarin.Essentials.Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    coordinates.Latitude = location.Latitude;
+                    coordinates.Longitude = location.Longitude;
+                }
+                else
+                {
+                    coordinates.Latitude = 0;
+                    coordinates.Longitude = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+
+            return coordinates;
+        }
+    }
+}
---
+++ WeatherApp/Services/WeatherService.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using WeatherApp.NETMAUI.Models;
+
+namespace WeatherApp.NETMAUI.Services
+{
+    public class WeatherService
+    {
+        HttpClient _client;
+        public WeatherService()
+        {
+            _client = new HttpClient();
+        }
+        public async Task<CurrentWeather> GetCurrentWeather(string request)
+        {
+            CurrentWeather currentWeather = null;
+
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    currentWeather = JsonSerializer.Deserialize<CurrentWeather>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+            return currentWeather;
+        }
+    }
+}
